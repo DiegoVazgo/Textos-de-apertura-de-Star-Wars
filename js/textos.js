07beb063dcbe60f86529f863dd870cfd95f56d55
Diego Efren <defren3211@gmail.com>
2017-12-07 00:04:26 -0600

    Agregue archivos importantes para el proyecto

---
+++ js/textos.js
@@ -0,0 +1,64 @@
+    // une = Una Nueva Esperanza
+var une = {
+        name: 'Episodio IV',
+        title: 'Una Nueva Esperanza',
+        textoUno: 'Son tiempos de guerra civil. Naves rebeldes han atacado desde una base secreta y han obtenido su primera victoria contra el malvado Imperio Galáctico.',
+        textoDos: 'Durante la batalla, espías rebeldes lograron robar los planos secretos del arma más extrema del Imperio, la ESTRELLA DE LA MUERTE, una estación espacial blindada con suficiente potencia para destruir un planeta entero.',
+        textoTres: 'Perseguida por los siniestros agentes del Imperio, la Princesa Leia se dirige velozmente a casa en su nave espacial, mientras resguarda los planos que pueden salvar a su pueblo y restaurar la libertad en la galaxia...'
+    },
+    //eic = El Imperio Contraataca
+    eic = {
+        name: 'Episodio V',
+        title: 'El Imperio Contraataca',
+        textoUno: 'Es una época oscura para la Rebelión. Aunque la Estrella de la Muerte ha sido destruida, las tropas imperiales han obligado a las fuerzas rebeldes a dejar sus bases secretas y las han perseguido a través de la galaxia.',
+        textoDos: 'En una fuga de la temida Flota Imperial, un grupo de guerreros rebeldes liderado por Luke Skywalker ha establecido una nueva base secreta en la remota tierra congelada de Hoth.',
+        textoTres: 'Darth Vader, Señor del mal, Obsesionado con encontrar al joven Skywalker, ha despachado al lejano espacio miles de sondas a control remoto...'
+    },
+    //erdj = El Retorno del Jedi
+    erdj = {
+        name: 'Episodio VI',
+        title: 'El Retorno del Jedi',
+        textoUno: 'Luke Skywalker ha regresado a Tatooine, su planeta de origen, en un intento de rescatar a su amigo Han Solo de las garras del vil Jabba.',
+        textoDos: 'Luke no sabe que el IMPERIO GALÁCTICO ha iniciado secretamente la construcción de una nueva estación espacial blindada, incluso más poderosa que la primera y temida Estrella de la Muerte.',
+        textoTres: 'Una vez terminada, esta arma extrema augurará una muerte segura para el pequeño grupo de rebeldes que lucha por restaurar la libertad en la galaxia...'
+    },
+    //laf = La Amenaza Fantasma
+    laf = {
+        name: 'Episodio I',
+        title: 'La Amenaza Fantasma',
+        textoUno: 'La República Galáctica está sumida en disturbios. Hay protestas contra la tributación de las rutas comerciales a sistemas estelares.',
+        textoDos: 'Esperando resolver el problema con un bloqueo de mortíferos cruceros, la avariciosa Federación de Comercio ha detenido todos los envios al pequeño planeta de Naboo.',
+        textoTres: 'Mientras el Congreso de la República debate sin fin esta alarmante cadena de acontecimientos, el Canciller Supremo ha despachado en secreto a dos Caballeros Jedi, los guardianes de la paz y la justicia en la galaxia, a resolver el conflicto...'
+    },
+    //eadlc = El Ataque de los Clones
+    eadlc = {
+        name: 'Episodio II',
+        title: 'El Ataque de los Clones',
+        textoUno: 'Hay inquietud en el Senado Galáctico. Varios miles de sistemas solares han declarado sus intenciones de abandonar la República.',
+        textoDos: 'El movimiento separatista, bajo el liderazgo del misterioso Conde Dooku, ha hecho difícil que el número limitado de Caballeros Jedi mantengan la paz y el orden en la galaxia.',
+        textoTres: 'La Senadora Amidala, la ex reina de Naboo, va a regresar al Senado Galáctico para votar sobre la cuestión crítica de formar un EJÉRCITO DE LA REPÚBLICA para ayudar a los abrumados Jedi...'
+    },
+    //lvdls = La Venganza de los Sith
+    lvdls = {
+        name: 'Episodio III',
+        title: 'La Venganza de los Sith',
+        textoUno: '¡Guerra! La República se desmorona bajo los ataques del despiadado Conde Dooku, señor de los Sith. Hay héroes en ambos bandos. El mal está por doquier.',
+        textoDos: 'En una maniobra audaz, el diabólico líder droide, general Grievous, ha entrado a la capital de la República y secuestrado al canciller Palpatine, líder del Senado Galáctico.',
+        textoTres: 'Mientras el Ejército Droide Separatista trata de huir de la asediada capital con su valioso rehén, dos caballeros Jedi dirigen una misión desesperada para rescatar al canciller cautivo...'
+    },
+    //eddlf = El Despertar de la Fuerza
+    eddlf = {
+        name: 'Episodio VII',
+        title: 'El Despertar de la Fuerza',
+        textoUno: 'Luke Skywalker ha desaparecido. En su ausencia, la siniestra PRIMERA ORDEN ha surgido de las cenizas del Imperio y no descansará hasta que Skywalker, el último Jedi, haya sido destruido.',
+        textoDos: 'Con el apoyo de la REPÚBLICA, la General Leia Organa dirige una valiente RESISTENCIA. Desesperadamente busca a su hermano Luke con el fin de obtener su ayuda para restaurar la paz y la justicia en la galaxia.',
+        textoTres: 'Leia ha enviado a su piloto más audaz en una misión secreta a Jakku, donde un viejo aliado ha descubierto una pista del paradero de Luke...'
+    },
+    //luj = Los Últimos Jedi
+    luj = {
+        name: 'Episodio VIII',
+        title: 'Los Últimos Jedi',
+        textoUno: '*Sin Información*',
+        textoDos: '*Sin Información*',
+        textoTres: '*Sin Información*'
+    };
